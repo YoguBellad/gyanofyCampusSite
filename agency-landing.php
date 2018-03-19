382e2884c4b3e4d7ca042ca4a7b8619fd1d1bc17
Yogu Bellad <yogugithub@gmail.com>
2018-03-19 10:26:27 +0530

    Add files via upload

---
+++ agency-landing.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+       
+        <!-- CSS Dependencies -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+      
+        <link rel="stylesheet" href="css/shards.min.css">
+        <link rel="stylesheet" href="css/shards-extras.min.css">
+    </head>
+    
+  
+    <body class="shards-landing-page--1">
+      <!-- Welcome Section -->
+      
+      <!-- / Welcome Section -->
+
+      <!-- Our Services Section -->
+      <div id="our-services" class="our-services section py-4">
+        <h3 class="section-title text-center my-5">Our Services</h3>
+        <!-- Features -->
+        <div class="features py-4 mb-4">
+          <div class="container">
+            <div class="row">
+              <div class="feature py-4 col-md-6 d-flex">
+                <div class="icon text-primary mr-3"><i class="fa fa-paint-brush"></i></div>
+                <div class="px-4">
+                    <h5>Design & Branding</h5>
+                    <p>Quisque mollis mi ac aliquet accumsan. Sed sed dapibus libero. Nullam luctus purus duis sensibus signiferumque.</p>
+                </div>
+              </div>
+              <div class="feature py-4 col-md-6 d-flex">
+                <div class="icon text-primary mr-3"><i class="fa fa-code"></i></div>
+                <div class="px-4">
+                    <h5>Programming</h5>
+                    <p>Quisque mollis mi ac aliquet accumsan. Sed sed dapibus libero. Nullam luctus purus duis sensibus signiferumque.</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="feature py-4 col-md-6 d-flex">
+                <div class="icon text-primary mr-3"><i class="fa fa-font"></i></div>
+                <div class="px-4">
+                    <h5>Copywriting</h5>
+                    <p>Quisque mollis mi ac aliquet accumsan. Sed sed dapibus libero. Nullam luctus purus duis sensibus signiferumque.</p>
+                </div>
+              </div>
+              <div class="feature py-4 col-md-6 d-flex">
+                <div class="icon text-primary mr-3"><i class="fa fa-support"></i></div>
+                <div class="px-4">
+                    <h5>Training & Support</h5>
+                    <p>Quisque mollis mi ac aliquet accumsan. Sed sed dapibus libero. Nullam luctus purus duis sensibus signiferumque.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- / Features -->
+      </div>
+      <!-- / Our Services Section -->
+
+  
+      <!-- / Footer Section -->
+
+      <!-- JavaScript Dependencies -->
+     
+    </body>
+</html>
---
+++ indexXX.php
@@ -0,0 +1,263 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Gyanofy@Campus</title>
+    <meta name="description" content="Elegant UI Toolkit for Professional Web Creatives Based on Bootstrap 4">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/shards.min.css">
+    <link rel="stylesheet" href="css/shards-demo.css?v=1.1.0">
+    <link rel="icon" href="favicon.ico">
+    
+    
+    
+    
+        <!-- Hero area slider css Timer Start-->
+        <link rel="stylesheet" href="plugins/slider/slider.css">
+        
+        <!-- Fancybox -->
+        <link rel="stylesheet" href="css/style.css">
+    
+        <!-- Hero area slider css Timer END-->
+    
+    
+</head>
+<style>
+    .navbar-light .navbar-nav .nav-link {
+    color: rgba(0,0,0,08);
+    font-weight: 300;
+        font-size: 1em;
+} 
+    
+   
+    
+    
+    .navbar-light .navbar-nav .active>.nav-link, .navbar-light .navbar-nav .nav-link.active, .navbar-light .navbar-nav .nav-link.show, .navbar-light .navbar-nav .show>.nav-link {
+    color: rgba(0,0,0,.9);
+    font-weight: 600;
+}
+    
+    </style>
+<body>
+    
+            <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light mb-4">
+                <img src="images/demo/logo.png" alt="Example Navbar 1" class="mr-2" height="50px">
+                
+                <div class="headcamp">
+                <a class="navbar-brand" href="#">@ Campus</a>
+                
+                </div>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown-7"
+                        aria-controls="navbarNavDropdown-7"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown-7">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home
+                                <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="https://designrevision.com" id="navbarDropdownMenuLink-7"
+                               data-toggle="dropdown" aria-haspopup="true"
+                               aria-expanded="false">
+                                Services
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink-7">
+                                <a class="dropdown-item" href="#">Design</a>
+                                <a class="dropdown-item" href="#">Development</a>
+                                <a class="dropdown-item" href="#">Marketing</a>
+                            </div>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa fa-twitter"></i></a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa fa-facebook"></i></a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa fa-linkedin"></i></a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa fa-github"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+    
+<div class="loader"><div class="page-loader"></div></div>
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+<!-- Floating Shards  -->
+<div class="leaf">
+    <img src="images/demo/slbk6.png" alt="Grow" class="shard"   >
+</div>
+<!-- Welcome Section -->
+<div class="welcome d-flex justify-content-center flex-column">
+    
+    <div class="inner-wrapper mt-auto mb-auto">
+        
+        <h2 class="slide-in"><br><br><br><br><br><b>Gyanofy</b> @ Campus</h2>
+        
+        
+           
+    
+    <!-- Timer Start  -->
+   
+    
+       
+                    <h3 class="wow fadeInUp animated cd-headline slide" data-wow-delay=".4s" >
+                    <span>Hi, a child could be </span>
+                    <span class="cd-words-wrapper">
+                        <b class="is-visible">Artist          </b>
+                        <b>CEO     </b>
+                        <b>President    </b>
+                        <b>Entreprenuer     </b>
+                        <b>Designer     </b>
+                        <b>Developer    </b>
+                        <b>Charted Accountant</b>
+                        <b>Agriculturist</b>
+                        <b>Detective    </b>
+                        <b>Executive    </b>
+                        <b>Surgeon      </b>
+                        <b>Pilot        </b>
+                        <b>Scientist    </b>
+                        <b>Sportsman    </b>
+                        <b>Doctor    </b>
+                        <b>Engineer     </b>
+                        
+                    </span>
+                    </h3>
+        
+        
+        
+        
+        
+    <!-- Timer End  -->
+    
+    
+        
+        <p class="slide-in"><strong>Modern tech & tools brought in radar of makers &amp; doers</strong></p>
+        
+        <div class="action-links slide-in">
+          <a href="#" data-scroll-to="#introduction" id="scroll-to-content" class="btn btn-primary btn-pill align-self-center mr-2"><i class="fa fa-newspaper mr-1">    </i>  Learn More</a>
+            
+            
+            
+        </div>
+    </div>
+    
+    
+ 
+    
+    
+   
+</div>
+
+<!-- Page Content -->
+<div class="page-content">
+    <!-- Content -->
+    <div class="content clearfix">
+
+        <div id="introduction" class="container mb-5">
+            <div class="section-title col-lg-8 col-md-10 ml-auto mr-auto">
+                <p class="lead">Shards is a modern design system based on Bootstrap 4 that comes packed with <b>10 extra
+                    custom components</b> and <b>two pre-built landing pages</b>. It’s also lightweight with its
+                    stylesheet weighting only <b>~12kb minified and gzipped</b>.</p>
+              
+            </div>
+        </div>
+
+        
+        
+        <?php include('agency-landing.php')?>
+        
+
+
+
+    <footer class="main-footer py-5">
+
+         <p class="text-muted text-center small p-0 mb-4">&copy; Copyright 2017 — Gyanofy</p>
+
+        <div class="social d-table ml-auto mr-auto">
+           
+            
+            <a class="twitter mx-3 h4" href="https://twitter.com/DesignRevision" target="_blank">
+                <i class="fa fa-twitter"></i>
+                <span class="sr-only">View our Twitter Profile</span>
+            </a>
+            <a class="facebook mx-3 h4" href="https://www.facebook.com/designrevision" target="_blank">
+                <i class="fa fa-facebook"></i>
+                <span class="sr-only">View our Facebook Profile <span>
+            </a>
+            <a class="github mx-3 h4" href="https://github.com/designrevision" target="_blank">
+                <i class="fa fa-github"></i>
+                <span class="sr-only">View our GitHub Profile</span>
+            </a>
+        </div>
+
+</footer>
+</div>
+
+<!-- JavaScript -->
+<div id="fb-root"></div>
+<script>(function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=1662270373824826"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));</script>
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+<script src="js/shards.min.js"></script>
+<script src="js/demo.min.js"></script>
+
+        
+        
+        
+        
+        
+        
+        	<!-- Timer STart -->
+<!--jquery -->
+	<script src="plugins/jQurey/jquery.min.js"></script>
+	<!-- slider js -->
+	<script src="plugins/slider/slider.js"></script>
+
+        	<!-- Timer END-->
+        
+        
+        
+        
+        
+    
+        
+</body>
+</html>
